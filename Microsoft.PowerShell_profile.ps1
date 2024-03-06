ef37f238061f0e6518914f9f02d00a2f0a0f9eb5
rne1223 <rne1223@gmail.com>
2024-03-06 00:10:39 -0800

    Added Get-PlaylistLink and other things

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -8,11 +8,24 @@ import-module oh-my-posh
 
 oh-my-posh --init --shell pwsh --config "$(scoop prefix oh-my-posh3)\themes\jandedobbeleer.omp.json" | Invoke-Expression
 
-New-Alias "hfcli" huggingface-cli
+# New-Alias "hfcli" huggingface-cli
 # New-Alias -Name s -Value { Start-Process -NoNewWindow @args } -Force
 
 function usp {
-	start "$(cd 'C:\Users\rne1223\projects\unreal'; fd -e uproject | fzf)"
+	$exec = "$(Set-Location 'C:\Users\rne1223\projects\unreal'; fd -e uproject -L | fzf)"
+
+    if (!$exec) {
+        Write-Output "Please select an Unreal Project"  -ForegroundColor Red
+        return        
+    }
+
+    # Run the project
+	Start-Process $exec
+
+	# Extract the directory path 
+	$directoryPath = Split-Path -Path $exec
+    # CD to Project Directory
+	Set-Location $directoryPath
 }
 
 # https://stackoverflow.com/questions/57318124/getting-pid-from-within-a-start-job
@@ -21,10 +34,31 @@ function usp {
 #	(Start-Process "C:\Users\rne1223\scoop\shims\mpv.exe" -PassThru -ArgumentList "--playlist='C:\Users\rne1223\playlist' --no-video --volume=50").Id
 #}
 
+#########################
+### Alias  ##############
+#########################
+
+function Get-PlaylistLink($csv = "C:\Users\rne1223\playlist"){
+   $csvData = Import-Csv -Path $csv 
+
+    $firstColumnHeader = ($csvData | Get-Member -MemberType NoteProperty).Name[0]
+    # Select the first column by its header
+    $firstColumn = $csvData | Select-Object -ExpandProperty $firstColumnHeader
+    # Output the first column's data
+    $test = $firstColumn | fzf
+
+    # Access column data using the column header as the property name
+    foreach ($row in $csvData) {
+        if ($row.song_name -eq $test) {
+            return $row.url 
+        }
+    }
+}
+
 function playm($Volume = "50")
 {
 	#Start-Job -ScriptBlock { C:\Users\rne1223\scoop\shims\mpv.exe --playlist='C:\Users\rne1223\playlist' --no-video --volume=30}
-	C:\Users\rne1223\scoop\shims\mpv.exe --no-video --volume=30 "$(cat C:\Users\rne1223\playlist | fzf)" 
+	C:\Users\rne1223\scoop\shims\mpv.exe --no-video --volume=30 "$(Get-PlaylistLink)"
 }
 
 function kills(){
@@ -70,18 +104,39 @@ function updateVscode ($zipFilePath) {
 }
 
 # Function to be use with fd to find files with a time input 
-function fdt([int]$time=1){
+function fdt([string]$time="1hour"){
 
-	$currentTime = Get-Date 
-	$elapsedTime = $currentTime.AddHours(-$time) ## Subtract the value of $lapsed from $currentTime
-	$finalTime= $elapsedTime.ToString("yyyy-MM-dd") 
-	fd -d 1 --newer $finalTime
+	#$currentTime = Get-Date 
+	#$elapsedTime = $currentTime.AddHours(-$time) ## Subtract the value of $lapsed from $currentTime
+	#$finalTime= $elapsedTime.ToString("yyyy-MM-dd") 
+	fd -d 1 --newer "$time"
 } 
+
+function cb (){
+	$filepath = Get-Location | set-clipboard
+	return $filepath
+} 
+
+function cbf ($file) {
+
+    process {
+        $inputValue = $_
+        Write-Host "Processing: $inputValue"
+    }
+	
+}
+
+function dar() {
+    Start-Process "D:\Videos\DavinciProjects\Project.drb"
+}
+
+
+
 
 #region conda initialize
 # !! Contents within this block are managed by 'conda init' !!
  If (Test-Path "C:\Users\rne1223\scoop\apps\miniconda3\current\Scripts\conda.exe") {
-     (& "C:\Users\rne1223\scoop\apps\miniconda3\current\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
+     (& "C:\Users\rne1223\scoop\apps\miniconda3\current\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Where-Object {$_} | Invoke-Expression
      }
 #     #endregion
 
